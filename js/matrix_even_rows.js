047172463120a5c45e5a9d5b779f1be9c2638ec1
elias <elias@ustisha.ru>
2024-12-25 16:41:11 +0700

    6 laba

---
+++ js/matrix_even_rows.js
@@ -0,0 +1,23 @@
+function create_matrix(rows_id, cols_id) {
+    let rows = parseInt(document.getElementById(rows_id).value)
+    let cols = parseInt(document.getElementById(cols_id).value)
+    
+    let matrix = []
+
+    for (let i = 0; i < rows; i++) {
+        let arr = []
+        for (let j = 0; j < cols; j++) {
+            let random_number = Math.floor(Math.random() * 51);
+            arr.push(random_number)
+        }
+        matrix.push(arr)
+    }
+    console.log(matrix)
+    return matrix;
+}
+
+function output_even_rows(matrix, res_id){
+    for (let i = 0; (i+1) < matrix.length; i++){
+        document.getElementById(res_id).innerHTML = matrix[i+1] + " | "
+    }
+}
