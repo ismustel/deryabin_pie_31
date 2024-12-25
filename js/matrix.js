f2b60eeeb122e2b0bae0409e6ae0b0e4114e77d3
elias <elias@ustisha.ru>
2024-12-25 14:16:14 +0700

    last

--- js/matrix.js
+++ js/matrix.js
@@ -2,28 +2,31 @@ function create_matrix(rows_id, cols_id) {
     let rows = parseInt(document.getElementById(rows_id).value)
     let cols = parseInt(document.getElementById(cols_id).value)
     
-    let matrix = new Array(rows)
+    let matrix = []
 
     for (let i = 0; i < rows; i++) {
-        matrix[i] = new Array(cols).fill(0)
+        let arr = []
         for (let j = 0; j < cols; j++) {
-            matrix[i][j] = Math.floor(Math.random() * 51);
+            let random_number = Math.floor(Math.random() * 51);
+            arr.push(random_number)
         }
+        matrix.push(arr)
     }
+    console.log(matrix)
     return matrix;
 }
 
 function output_string_elements(matrix, k_id, res_id) {
     let k = parseInt(document.getElementById(k_id).value)
 
-    let res = ""
+    let res_string = ""
 
     if (k >= 1 || k <= matrix.length) {
         k -= 1
         for (let n = 0; n < matrix[k].length; n++) {
-            res += matrix[k][n] + " "
+            res_string += matrix[k][n] + " "
         }
-        document.getElementById(res_id).innerHTML = res
+        document.getElementById(res_id).innerHTML = res_string
         
     } else {
         document.getElementById(res_id).innerHTML = "Введен неверный номер строки"
